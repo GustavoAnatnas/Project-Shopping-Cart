52a2b75c2c0063b9e9d3d6889064af4dca57985f
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-03-29 19:09:56 -0300

    Fixa lint e possiveis erros

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  event();
+}
+
+function createCartItemElement({ name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `${name} \n $${salePrice} \n\n`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const getProductByID = async (event) => {
+  const getId = event.target.parentNode.firstChild.innerText;
+  const productId = await fetchItem(getId);
+
+    const produts = createCartItemElement({
+      sku: productId.id, 
+      name: productId.title, 
+      salePrice: productId.price,
+     });
+     const cartItems = document.querySelector('.cart__items');
+     cartItems.appendChild(produts); 
+  };
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,21 +51,9 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 const getElements = async () => {
   const products = await fetchProducts('computador');
@@ -57,20 +70,6 @@ const getElements = async () => {
   });
 };
 
-const getProductByID = async (event) => {
-  const getId = event.target.parentNode.firstChild.innerText;
-  const productId = await fetchItem(getId);
-
-    const produts = createCartItemElement({
-      sku: productId.id, 
-      name: productId.title, 
-      salePrice: productId.price,
-     });
-     const cartItems = document.querySelector('.cart__items');
-     cartItems.appendChild(produts); 
-  };
-
 window.onload = async () => { 
   await getElements();
-  addItens();
 };
